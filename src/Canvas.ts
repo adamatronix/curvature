f2d2442e3acae4e0659912a50daa4e347b098456
Your Name <you@example.com>
2021-10-18 22:38:35 +0800

    setup More setup shit

---
+++ src/Canvas.ts
@@ -0,0 +1,27 @@
+import * as P5 from 'p5';
+
+class Canvas {
+
+  container:HTMLDivElement;
+
+  constructor(container: HTMLDivElement) {
+    this.container = container;
+    new P5(this.sketch);
+  }
+
+  sketch = (p5: P5) => {
+    p5.setup = () => {
+      const canvas = p5.createCanvas(this.container.offsetWidth, this.container.offsetHeight);
+      canvas.parent(this.container);
+      canvas.style('position', 'absolute');
+      canvas.style('left', 0);
+      canvas.style('top', 0);
+      canvas.style('z-index', 1);
+      p5.frameRate(10);
+    }
+
+  }
+
+}
+
+export default Canvas;
